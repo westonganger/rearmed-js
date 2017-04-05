d1048948a2306fcfcf410567756f1c1fa4fa78b6
Weston Ganger <westonganger@gmail.com>
2017-04-05 15:01:20 -0700

    fix a couple warnings

--- dist/rearmed/string/startsWith.js
+++ dist/rearmed/string/startsWith.js
@@ -1,14 +1,11 @@
 (function(){
   "use strict";
 
-  var warn = require('./../core/warn');
-  if(String.prototype.startsWith){
-    warn(String, 'startsWith');
+  if(!String.prototype.startsWith){
+    String.prototype.startsWith = function(x){
+      return this.substr(0, x.length) === x;
+    };
+
+    Object.defineProperty(String.prototype, "startsWith", {enumerable: false});
   }
-
-  String.prototype.startsWith = function(x){
-    return this.substr(0, x.length) === x;
-  };
-
-  Object.defineProperty(String.prototype, "startsWith", {enumerable: false});
 }(this));
--- src/rearmed/string/endsWith.js
+++ src/rearmed/string/endsWith.js
@@ -1,14 +1,11 @@
 (function(){
   "use strict";
 
-  var warn = require('./../core/warn');
-  if(String.prototype.endsWith){
-    warn(String, 'endsWith');
+  if(!String.prototype.endsWith){
+    String.prototype.endsWith = function(x){
+      return this.substr((this.length - x.length), this.length) === x;
+    };
+
+    Object.defineProperty(String.prototype, "endsWith", {enumerable: false});
   }
-
-  String.prototype.endsWith = function(x){
-    return this.substr((this.length - x.length), this.length) === x;
-  };
-
-  Object.defineProperty(String.prototype, "endsWith", {enumerable: false});
 }(this));
