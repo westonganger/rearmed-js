b9a770ef6465d03308ce64f64e0691cccd322bd4
Bogadon <kazimierzz@gmail.com>
2017-07-11 14:53:59 +0100

    Add #presence method, as per Rails version

---
+++ array/presence.js
@@ -0,0 +1,28 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){
+  "use strict";
+
+  var warn = require('./../core/warn');
+  if(Array.prototype.presence){
+    warn('Array', 'presence');
+  }
+
+  Array.prototype.presence = function(){
+    return this.length > 0 ? this : null
+  };
+
+  Object.defineProperty(Array.prototype, "presence", {enumerable: false});
+}(this));
+
+},{"./../core/warn":2}],2:[function(require,module,exports){
+function warn(type, method, notPrototype){
+  if(type && method){
+    console.warn("Rearmed-js Overriding " + type + (notPrototype ? '.' : '.prototype.') + method, '. If this is a built-in browser method please report on Rearmed-js github issues.');
+  }else{
+    throw("incorrect number of arguments")
+  }
+};
+
+module.exports = warn;
+
+},{}]},{},[1]);
---
+++ number/presence.js
@@ -0,0 +1,28 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function warn(type, method, notPrototype){
+  if(type && method){
+    console.warn("Rearmed-js Overriding " + type + (notPrototype ? '.' : '.prototype.') + method, '. If this is a built-in browser method please report on Rearmed-js github issues.');
+  }else{
+    throw("incorrect number of arguments")
+  }
+};
+
+module.exports = warn;
+
+},{}],2:[function(require,module,exports){
+(function(){
+  "use strict";
+
+  var warn = require('./../core/warn');
+  if(Number.prototype.presence){
+    warn('Number', 'presence');
+  }
+
+  Number.prototype.presence = function(){
+    return this; // in Rails: 0, NaN and Infinities are all present?
+  };
+
+  Object.defineProperty(Number.prototype, "presence", {enumerable: false});
+}(this));
+
+},{"./../core/warn":1}]},{},[2]);
